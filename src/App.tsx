28f8dc379dfd6663e21bb0ba4803e0fbb61520be
Iliya <i.dontsov61rus@yandex.ru>
2022-08-02 15:11:53 +0300

    main

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Product from "./components/Product"
+
+function App() {
+  return (
+    <div className="container mx-auto max-w-7xl pt-5">
+      <Product />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Product.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Product() {
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      Product
+    </div>
+  )
+}
+
+export default Product
